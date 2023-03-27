80394fccc9d5413fa7c813d583f58d3fad0e5bff
Valentin <1032202474@pfur.ru>
2023-03-27 11:03:08 +0300

    updating api client

--- java/ApiClient/src/main/java/com/vdorofeev/apiclient/ApiClientApplication.java
+++ java/ApiClient/src/main/java/com/vdorofeev/apiclient/ApiClientApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiClientApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiClientApplication.class, args);
 	}
 
 }
+
+
--- java/ApiClient/src/main/java/com/vdorofeev/apiclient/controller/RequestController.java
+++ java/ApiClient/src/main/java/com/vdorofeev/apiclient/controller/RequestController.java
@@ -1,28 +1,43 @@
 package com.vdorofeev.apiclient.controller;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 @RestController
 @RequestMapping
 public class RequestController {
-    String pathToPythonFile = "/home/vdorofeev/PlagiarismChecker/python/check_code.py";
+
+    @Value("${arg}")
+    private String myProperty;
+
     @PostMapping("checkcode")
-    public String getResponce(@RequestBody String path) throws IOException {
+    public String getResponce(@RequestBody String path) throws IOException, ParseException {
         return executePython(path);
     }
 
     @GetMapping("ping")
-    public String checkAlive() throws IOException {
+    public String checkAlive() throws IOException, ParseException {
         return executePython("ping");
     }
 
-    private String executePython(String argument) throws IOException {
+    private String executePython(String argument) throws IOException, ParseException {
+        String pathToConfig = this.myProperty;
+        Object obj = new JSONParser().parse(new FileReader(pathToConfig));
+        JSONObject jsonObject = (JSONObject) obj;
+        String pathToPythonScript = (String) jsonObject.get("pathToPythonScript");
+        String pathToIndexes = (String) jsonObject.get("pathToIndexes");
+
         ProcessBuilder builder = new ProcessBuilder("python",
-                this.pathToPythonFile, argument);
+                pathToPythonScript, argument, pathToIndexes);
         Process process = builder.start();
+
         Scanner output = new Scanner(process.getInputStream()).useDelimiter("\\A");
         Scanner errors = new Scanner(process.getErrorStream()).useDelimiter("\\A");
         String result_err = errors.hasNext() ? errors.next() : "";
